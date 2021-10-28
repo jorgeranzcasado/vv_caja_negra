007bb73d3248ef597199cbe58f09c1f6dd9530ab
jorgeranzcasado <63307023+jorgeranzcasado@users.noreply.github.com>
2021-10-28 11:17:08 +0200

    Pruebas reverse

---
+++ src/test/java/com/practica/cajanegra/reverse.java
@@ -0,0 +1,41 @@
+package com.practica.cajanegra;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.cajanegra.SingleLinkedListImpl;
+
+public class Reverse {
+
+    private AbstractSingleLinkedListImpl<String> lista;
+
+    @BeforeEach
+    public void setUp(){
+        this.lista = new SingleLinkedListImpl<String>("A","B","C"); //Varios elementos
+        this.lista = new SingleLinkedListImpl<String>("A"); //Un elemento
+        this.lista = new SingleLinkedListImpl<String>(); //Sin elementos
+    }
+    //Probamos su funcionamiento
+    @Test
+    public void testReverse(){
+        this.lista = this.lista.reverse();
+        assertEquals("[C, B, A]", this.lista.toString());
+    }
+    //Probamos con un elemento
+    @Test
+    public void testUnElemento(){
+        this.lista = this.lista.reverse();
+        assertEquals("[A]", this.lista.toString());
+    }
+    //Probamos con la lista vacia
+    @Test
+    public void testVacio(){
+        this.lista = this.lista.reverse();
+        assertEquals("[]", this.lista.toString());
+    }
+}
+
