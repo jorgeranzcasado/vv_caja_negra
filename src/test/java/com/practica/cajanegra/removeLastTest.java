dbf1f608f1300ee7e08d14ba2eed69569d458774
hfarooqupm <78760561+hfarooqupm@users.noreply.github.com>
2021-11-08 16:05:11 +0100

    Añadido Test Vacio de RemoveLast

--- src/test/java/com/practica/cajanegra/removeLastTest.java
+++ src/test/java/com/practica/cajanegra/removeLastTest.java
@@ -3,29 +3,24 @@ package com.practica.cajanegra;
 import com.cajanegra.EmptyCollectionException;
 import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.Test;
-
-
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
-
-import org.junit.*;
-import org.junit.jupiter.api.Test;
-import com.cajanegra.AbstractSingleLinkedListImpl;
-import com.cajanegra.SingleLinkedListImpl;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import org.junit.*;
+import com.cajanegra.AbstractSingleLinkedListImpl;
 
 
+public class removeLastTest {
 
-public class removeLastTest{
+
     @Test
 
     public void testremovelast() throws EmptyCollectionException {
-
+// Si hay varios elementos en la lista, tambien lo elimina?
         SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
         assertEquals("D",mylist.removeLast());
 
@@ -33,34 +28,37 @@ public class removeLastTest{
     }
 
 
-
+    @Test
+    public void testRemovelastUnElemento() throws EmptyCollectionException {
+//probamos con un elemento de la lista
+        SingleLinkedListImpl<String> miList = new SingleLinkedListImpl<String>("C");
+        // La lista deja añadir valores fuera de rango que es de A hasta Z, en este caso deja enteros
+        assertTrue(miList.removeLast()=="C");
+        System.out.println(miList);
+    }
 
 
 
     @Test
-    public void testRemovelastinteger() throws EmptyCollectionException {
+    // Probamos con varios elementos si elimina el ultimo elemento si tenemos la lista con mas de un elemento
+    public void testremoveLastVariosElementos() throws EmptyCollectionException{
+        SingleLinkedListImpl<String> miList = new SingleLinkedListImpl<String>("A","B","Z");
+        assertEquals("Z",miList.removeLast());
+        System.out.println(miList);
 
-        SingleLinkedListImpl<Integer> mylist = new SingleLinkedListImpl<Integer>(56,28,60,59,82);
-
-        // La lista deja añadir valores fuera de rango que es de A hasta Z, en este caso deja enteros
-        //
-        int devuelve=mylist.removeLast();
-        assertEquals(82,devuelve);
-        System.out.println(mylist);
     }
 
     // Si la lista esta vacia salta la excepcion de que la lista esta vacia.
     @Test
-    public void testRemovelastEmpty() throws EmptyCollectionException {
+    public void testRemovelastVACIO() {
 
-        SingleLinkedListImpl<Integer> mylist = new SingleLinkedListImpl<Integer>();
+        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>();
+        // assertEquals("Z",mylist.removeLast());
+        Exception exception = assertThrows(com.cajanegra.EmptyCollectionException.class, () -> mylist.removeLast());
+        assertEquals("La colecci\u00f3n La lista est\u00e1 vac\u00eda est\u00e1 vac\u00eda.", exception.getMessage());
 
         // Si es una lista vacia entonces salta la excepcion que esta vacio.
 
-        int devuelve=mylist.removeLast();
-        assertEquals("A",devuelve);
-        System.out.println(mylist);
+
     }
-
-
 }
