d264a4dbc8d9ca420212acf1c6e2cabf879c8114
hfarooqupm <78760561+hfarooqupm@users.noreply.github.com>
2021-11-08 10:19:58 +0100

    Removelast añadidos mas pruebas

--- src/test/java/com/practica/cajanegra/removeLastTest.java
+++ src/test/java/com/practica/cajanegra/removeLastTest.java
@@ -2,28 +2,54 @@ package com.practica.cajanegra;
 
 import com.cajanegra.EmptyCollectionException;
 import com.cajanegra.SingleLinkedListImpl;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 
-import static org.junit.Assert.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
 import org.junit.*;
+import org.junit.jupiter.api.Test;
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import com.cajanegra.SingleLinkedListImpl;
 
-public class removeLastTest {
+import com.cajanegra.EmptyCollectionException;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class removeLastTest{
     @Test
-    public void testRemovelast() throws EmptyCollectionException {
+
+    public void testremovelast() throws EmptyCollectionException {
+
+        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
+        assertEquals("D",mylist.removeLast());
+
+
+    }
+
+
+
+
+
+
+    @Test
+    public void testRemovelastinteger() throws EmptyCollectionException {
 
         SingleLinkedListImpl<Integer> mylist = new SingleLinkedListImpl<Integer>(56,28,60,59,82);
-        // permite meter valores fuera del rango que es de A hasta Z
-     
-        // lo que vamos a comproabr si el metodo removeLast elimina de verdad el ultimo  elemento en la lista pasado como parametro.
-        // deleting last element by passing index
-        int devuelve=mylist.removeLast(59);
-        assertEquals(59,devuelve);
+
+        // La lista deja añadir valores fuera de rango que es de A hasta Z, en este caso deja enteros
+        //
+        int devuelve=mylist.removeLast();
+        assertEquals(82,devuelve);
         System.out.println(mylist);
     }
+
+    // Si la lista esta vacia salta la excepcion de que la lista esta vacia.
     @Test
     public void testRemovelastEmpty() throws EmptyCollectionException {
 
@@ -31,8 +57,8 @@ public class removeLastTest {
 
         // Si es una lista vacia entonces salta la excepcion que esta vacio.
 
-        int devuelve=mylist.removeLast(59);
-        assertEquals(59,devuelve);
+        int devuelve=mylist.removeLast();
+        assertEquals("A",devuelve);
         System.out.println(mylist);
     }
 
