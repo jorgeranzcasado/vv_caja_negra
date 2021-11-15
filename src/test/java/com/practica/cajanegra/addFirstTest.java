66a889cecb1cf8fd92212f46f2dca042b79273c1
mrleguey <78793956+mrleguey@users.noreply.github.com>
2021-11-15 18:25:16 +0100

    Update addFirstTest.java

--- src/test/java/com/practica/cajanegra/addFirstTest.java
+++ src/test/java/com/practica/cajanegra/addFirstTest.java
@@ -1,7 +1,7 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,33 +9,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class addFirstTest {
     private SingleLinkedListImpl<String> miLista;
 
-    public addFirstTest() {
-    }
-
     @BeforeEach
     public void setUp() {
-        this.miLista = new SingleLinkedListImpl(new String[]{"A", "B", "C"});
+        this.miLista = new SingleLinkedListImpl<String>("A", "B", "C");
     }
 
-    @ParameterizedTest(
-        name = "Add First {0} in list"
-    )
-    @ValueSource(
-        strings = {"A", "B", "M", "Y", "Z"}
-    )
+    //Prueba de clases válidas a traves de test parametrizados
+    @ParameterizedTest( name = "Add First {0} in list")
+    @ValueSource( strings = {"A", "B", "M", "Y", "Z"})
     public void addFirstValido(String s) {
         this.miLista.addFirst(s);
-        Assertions.assertEquals("[" + s + ", A, B, C]", this.miLista.toString());
+        assertEquals("[" + s + ", A, B, C]", this.miLista.toString());
     }
 
-    @ParameterizedTest(
-        name = "Add First {0} in list"
-    )
-    @ValueSource(
-        strings = {"@", "["}
-    )
+    //Prueba de clases inválidas a través de test parametrizados
+    @ParameterizedTest( name = "Add First {0} in list")
+    @ValueSource( strings = {"@", "["})
     public void addFirstInvalido(String s) {
         this.miLista.addFirst(s);
-        Assertions.assertEquals("[A, B, C]", this.miLista.toString());
+        assertEquals("[A, B, C]", this.miLista.toString());
     }
 }
