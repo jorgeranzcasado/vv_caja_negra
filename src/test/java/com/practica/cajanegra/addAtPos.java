320dd7847691471257ece8013dc618004ea0e12d
luisrodriguezarriero <luis.rodriguez.arriero@alumnos.upm.es>
2021-11-15 13:41:06 +0100

    addAtPos y addLast acabados

--- src/test/java/com/practica/cajanegra/addAtPos.java
+++ src/test/java/com/practica/cajanegra/addAtPos.java
@@ -1,41 +1,41 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class addAtPos {
     private SingleLinkedListImpl<String> miLista;
-//Esta a medias, lo acabo en casa
+
     @BeforeEach
     public void setUp() {
         this.miLista = new SingleLinkedListImpl<>("A","B","C","D","E");
     }
 
-    // Clase valida:   1 < pos <= size()
+    //Clases válidas para string s: A-Z
     @ParameterizedTest(name= "Valid values - Add Element position {0} in list")
     @ValueSource(strings = {"@", "A", "B", "L", "Y", "Z", "["})
-    public void addAtPosMinMenos(String a) {
+    public void addAtPosMinMenos(String s) {
         boolean foundException = false;
         try{
-            this.miLista.addAtPos(a, 0);
-            foundException=true;
+            this.miLista.addAtPos(s, 0);
         }
         catch(java.lang.IllegalArgumentException e){
-
+            foundException=true;
         }
-        assertEquals(foundException, true);
+        assertTrue(foundException);
     }
 
-    @ParameterizedTest(name= "Valid values - Add Element position {2} in list")
+    @ParameterizedTest(name= "Valid values - Add Element position {1} in list")
     @ValueSource(strings = {"@", "A", "B", "L", "Y", "Z", "["})
     public void addAtPosMinPlus(String s) {
-        String[] salidaEsp = {s, "A","B","C", "D", "E"} ;
+        String salidaEsp = "[" + s + ", A, B, C, D, E]";
         this.miLista.addAtPos(s, 1);
         assertEquals(salidaEsp, this.miLista.toString());
     }
@@ -43,24 +43,16 @@ public class addAtPos {
     @ParameterizedTest(name= "Valid values - Add Element position {2} in list")
     @ValueSource(strings = {"@", "A", "B", "L", "Y", "Z", "["})
     public void addAtPosMed(String s) {
-        String[] salidaEsp = {s, "A","B","C", "D", "E"} ;
+        String salidaEsp = "[A, " + s + ", B, C, D, E]";
         this.miLista.addAtPos(s, 2);
         assertEquals(salidaEsp, this.miLista.toString());
     }
 
+    // Clases validas para index i:   1 < pos <= size()
     @ParameterizedTest(name= "Valid values - Add Element position {1} in list")
-    @ValueSource(strings = {"@", "A", "B", "L", "Y", "Z", "["}, ints = {300, Integer.MAX_VALUE-1, Integer.MAX_VALUE})
-    public void addAtPosMin(String s, int i) {
-        String[] salidaEsp = {"A","B","C", "D", "E", s} ;
-        this.miLista.addAtPos(s, i);
-        assertEquals(salidaEsp, this.miLista.toString());
-    }
-    //usar csvSDource porque ValueSource no funciona aqui
-
-    @ParameterizedTest(name= "Valid values - Add Element position {1} in list")
-    @ValueSource(strings = {"@", "["}, ints = {1, 2, 3 300, Integer.MAX_VALUE-1, Integer.MAX_VALUE})
+    @CsvFileSource(files = "datosMedMax-Max.csv")
     public void addAtPosElemInvalidosindexInvalido(String s, int i) {
-        String[] salidaEsp = {"A","B","C", "D", "E", s} ;
+        String salidaEsp = "[A, B, C, D, E, " + s +"]";
         this.miLista.addAtPos(s, i);
         assertEquals(salidaEsp, this.miLista.toString());
     }
---
+++ src/test/java/com/practica/cajanegra/addLast.java
@@ -0,0 +1,28 @@
+package com.practica.cajanegra;
+
+import com.cajanegra.SingleLinkedListImpl;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+public class addLast {
+    private SingleLinkedListImpl<String> miLista;
+
+    @BeforeEach
+    public void setUp() {
+        this.miLista = new SingleLinkedListImpl<>("A", "B", "C", "D", "E");
+    }
+
+    // Clase valida:   A - Z
+    @ParameterizedTest(name = "Valid values - Add Last robust test")
+    @ValueSource(strings = {"@", "A", "B", "L", "Y", "Z", "["})
+    public void addLastTest(String s) {
+        this.miLista.addLast(s);
+        String salidaEsp = "[A, B, C, D, E, "+ s + "]";
+        assertEquals(salidaEsp, this.miLista.toString());
+    }
+}
