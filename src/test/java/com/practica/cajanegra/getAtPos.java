2ab49a65c88a24c38faf3cdd429ccf753222440a
jorgeranzcasado <63307023+jorgeranzcasado@users.noreply.github.com>
2021-12-05 12:13:28 +0100

    Update getAtPos.java

--- src/test/java/com/practica/cajanegra/getAtPos.java
+++ src/test/java/com/practica/cajanegra/getAtPos.java
@@ -1,7 +1,6 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,27 +16,21 @@ public class getAtPos {
         this.miLista = new SingleLinkedListImpl<>("A","B","C","D","E");
     }
 
+    //Clase invalida:   pos <= 0
+    @Test
+    void getAtPostInvalido0() {
+        assertThrows(IllegalArgumentException.class, () -> this.miLista.getAtPos(0));
+    }
     // Clase valida:   0 < pos <= size()
     @ParameterizedTest(name= "Valid values - Get position {0} in list")
     @ValueSource(ints = {1,2,3,4,5})
     public void getAtPostValidos(int n) {
-        String[] salidaEsp = {"A","B","C","D","E"} ;
-        assertEquals(salidaEsp[n-1], this.miLista.getAtPos(n));
-    }
-
-    //Clase invalida:   pos <= 0
-    @Test
-    void getAtPostInvalido1() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-            this.miLista.getAtPos(0));
-        assertEquals("La posición no puede ser menor o igual a cero", exception.getMessage());
+        String[] salidaEsp = {"A", "B", "C", "D", "E"};
     }
 
     //Clase invalida:   pos > size()
     @Test
-    void getAtPostInvalido2() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                this.miLista.getAtPos(6));
-        assertEquals("La posición no puede ser superior al tamaño de la lista", exception.getMessage());
+    void getAtPostInvalido6() {
+        assertThrows(IllegalArgumentException.class, () -> this.miLista.getAtPos(6));
     }
 }
