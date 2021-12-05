508dde9c8039c994eb1dfc2ad931fdff94356531
jorgeranzcasado <63307023+jorgeranzcasado@users.noreply.github.com>
2021-12-05 13:35:25 +0100

    Update indexofTest.java

--- src/test/java/com/practica/cajanegra/indexofTest.java
+++ src/test/java/com/practica/cajanegra/indexofTest.java
@@ -1,26 +1,24 @@
 package com.practica.cajanegra;
 
-import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.AbstractSingleLinkedListImpl;
 import com.cajanegra.SingleLinkedListImpl;
 import static org.junit.jupiter.api.Assertions.*;
-import com.cajanegra.SingleLinkedListImpl;
-import java.util.Collections;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import java.util.NoSuchElementException;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class indexofTest {
-
-
-
+    private AbstractSingleLinkedListImpl<String> mylist;
+    @BeforeEach
+    public void setUp() {
+        this.mylist= new SingleLinkedListImpl<>("A","B","C","D");
+    }
     @Test
-// Lista con varios elementos y vamos a comprobar si devuelve bien el indice.
+    // Lista con varios elementos y vamos a comprobar si devuelve bien el indice.
     public void indexofVariosElementos() {
-
-        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
         assertEquals(1,mylist.indexOf("A"));
 
     }
@@ -30,41 +28,28 @@ public class indexofTest {
 
     @Test
     public void indexofVariosElementos2() {
-// Vamos a comrobar si devuelve indice de elemtnos de la lista que no sean los primeros
-        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
+    // Vamos a comrobar si devuelve indice de elemtnos de la lista que no sean los primeros
         assertEquals(2,mylist.indexOf("B"));
 
     }
 
-
     @Test
     public void indexofVariosElementos3() {
-// Vamos a comrobar si devuelve indice de elementos que estan en el medio
-        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
+    // Vamos a comrobar si devuelve indice de elementos que estan en el medio
         assertEquals(3,mylist.indexOf("C"));
 
     }
+
     @Test
     public void indexofVariosElementos4() {
-// Vamos a comrobar si devuelve indice del ultimo elemento de la lista
-        SingleLinkedListImpl<String> mylist = new SingleLinkedListImpl<String>("A", "B","C","D");
+    // Vamos a comrobar si devuelve indice del ultimo elemento de la lista
         assertEquals(4,mylist.indexOf("D"));
 
     }
 
     //Clase invalida:   elemento fuera de rango
     @Test
-
-
-        public void indexofInvalido2() {
-        SingleLinkedListImpl<String> miLista = new SingleLinkedListImpl<String>("A","B");
-
-        Exception exception = assertThrows (IllegalArgumentException.class, () -> miLista.indexOf("C"));
-
-            assertEquals("La posición no puede ser superior al tamaño de la lista", exception.getMessage());
-        }
-
-
-
-
+    public void indexofInvalido2() {
+        assertThrows (NoSuchElementException.class, () -> mylist.indexOf("E"));
+    }
 }
