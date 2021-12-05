eb32e924e339287d138a3d0dadb0b3ebdd1d987f
jorgeranzcasado <63307023+jorgeranzcasado@users.noreply.github.com>
2021-12-05 14:04:34 +0100

    Update addFirstTest.java

--- src/test/java/com/practica/cajanegra/addFirstTest.java
+++ src/test/java/com/practica/cajanegra/addFirstTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class addFirstTest {
-    private SingleLinkedListImpl<String> miLista;
+    private AbstractSingleLinkedListImpl<String> miLista;
 
     @BeforeEach
     public void setUp() {
