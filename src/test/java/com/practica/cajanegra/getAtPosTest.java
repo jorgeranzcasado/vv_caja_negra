6f77fb8af281caf30171343a9a9185db99ecab9b
eguadalupe <63119020+eguadalupe@users.noreply.github.com>
2021-12-11 20:39:53 +0100

    Update and rename getAtPos.java to getAtPosTest.java
    correccion de nombre

--- src/test/java/com/practica/cajanegra/getAtPosTest.java
+++ src/test/java/com/practica/cajanegra/getAtPosTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class getAtPos {
+public class getAtPosTest {
     private SingleLinkedListImpl<String> miLista;
 
     @BeforeEach
