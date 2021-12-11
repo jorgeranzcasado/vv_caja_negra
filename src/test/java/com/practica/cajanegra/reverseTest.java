35a4f59c53f3f5ae2ce0f8e7f09de8f83651c23b
jorgeranzcasado <63307023+jorgeranzcasado@users.noreply.github.com>
2021-12-11 20:45:51 +0100

    Update reverseTest.java

--- src/test/java/com/practica/cajanegra/reverseTest.java
+++ src/test/java/com/practica/cajanegra/reverseTest.java
@@ -13,27 +13,24 @@ public class reverseTest {
 
     private AbstractSingleLinkedListImpl<String> lista;
 
-    @BeforeEach
-    public void setUp(){
-        this.lista = new SingleLinkedListImpl<String>("A","B","C"); //Varios elementos
-        //this.lista = new SingleLinkedListImpl<String>("A"); //Un elemento
-        //this.lista = new SingleLinkedListImpl<String>(); //Sin elementos
-    }
     //Probamos su funcionamiento
     @Test
     public void testReverse(){
+        this.lista = new SingleLinkedListImpl<String>("A","B","C");
         this.lista = this.lista.reverse();
         assertEquals("[C, B, A]", this.lista.toString());
     }
     //Probamos con un elemento
     @Test
     public void testUnElemento(){
+        this.lista = new SingleLinkedListImpl<String>("A");
         this.lista = this.lista.reverse();
         assertEquals("[A]", this.lista.toString());
     }
     //Probamos con la lista vacia
     @Test
     public void testVacio(){
+        this.lista = new SingleLinkedListImpl<String>();
         this.lista = this.lista.reverse();
         assertEquals("[]", this.lista.toString());
     }
