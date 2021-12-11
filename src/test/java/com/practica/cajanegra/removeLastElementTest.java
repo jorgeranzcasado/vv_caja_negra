a82210bfd4afca3d4748375dfd3eea90e7f719de
eguadalupe <63119020+eguadalupe@users.noreply.github.com>
2021-12-11 19:35:36 +0100

    Update removeLastElementTest.java
    test corregido

--- src/test/java/com/practica/cajanegra/removeLastElementTest.java
+++ src/test/java/com/practica/cajanegra/removeLastElementTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class removeLastElementTest {
-    private AbstractSingleLinkedListImpl<String> miLista;
+  private AbstractSingleLinkedListImpl<String> miLista;
 
-    //Clase valida 1. A <= s <= Z – lista vacía (se lanza excepción)
+    //Clase valida 1. A <= s <= Z â€“ lista vacÃ­a (se lanza excepciÃ³n)
     @ParameterizedTest(name = "Eliminar Elemento {0} de una lista vacia")
     @ValueSource(strings = {"A", "B", "M", "Y", "Z"})
     public void removeElementVal1(String s) {
@@ -22,34 +22,37 @@ public class removeLastElementTest {
         });
     }
 
-    //Clase valida 2. A <= s <= Z – s no está en la lista (se lanza excepción)
+    //Clase valida 2. A <= s <= Z â€“ s no estÃ¡ en la lista (se lanza excepciÃ³n)
     @Test
     public void removeElementVal2() {
         this.miLista = new SingleLinkedListImpl<String>("A", "B", "C");
         assertThrows(NoSuchElementException.class, () -> {this.miLista.removeLast("D");});
     }
 
-    //Clase valida 3. A <= s <= Z – s está en la última posición de la lista
+    //Clase valida 3. A <= s <= Z â€“ s estÃ¡ en la Ãºltima posiciÃ³n de la lista
     @ParameterizedTest( name = "Eliminar Elemento {0} en la ultima pos de una lista")
     @ValueSource( strings = {"A", "B", "M", "Y", "Z"})
     public void removeElementVal3(String s) throws EmptyCollectionException {
         this.miLista = new SingleLinkedListImpl<String>("A", "B", "C", s);
         assertEquals(s, this.miLista.removeLast(s));
+        assertEquals("[A, B, C]", this.miLista.toString());
     }
 
-    //Clase valida 4. A <= s <= Z – s está en la primera posición de la lista
-    @ParameterizedTest( name = "Eliminar Elemento {0} en la primera posiciÃ³n de una lista")
+    //Clase valida 4. A <= s <= Z â€“ s estÃ¡ en la primera posiciÃ³n de la lista
+    @ParameterizedTest( name = "Eliminar Elemento {0} en la primera posicion de una lista")
     @ValueSource( strings = {"A", "B", "M", "Y", "Z"})
     public void removeElementVal4(String s) throws EmptyCollectionException {
         this.miLista = new SingleLinkedListImpl<String>(s, "A", "B", "C");
         assertEquals(s, this.miLista.removeLast(s));
+        assertEquals("[A, B, C]", this.miLista.toString());
     }
 
-    //Clase valida 5. A <= s <= Z – s está en el centro de la lista
+    //Clase valida 5. A <= s <= Z â€“ s estÃ¡ en el centro de la lista
     @ParameterizedTest( name = "Eliminar Elemento {0} en el centro de una lista")
     @ValueSource( strings = {"A", "B", "M", "Y", "Z"})
     public void removeElementVal5(String s) throws EmptyCollectionException {
         this.miLista = new SingleLinkedListImpl<String>("A", "B", s, "C", "D");
         assertEquals(s, this.miLista.removeLast(s));
+        assertEquals("[A, B, C, D]", this.miLista.toString());
     }
 }
